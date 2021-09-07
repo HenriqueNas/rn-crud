2e6a94882312271b4352a6b451a2b01c7472f6ae
YoungDevDotMp3 <brkshen@gmail.com>
2021-09-06 22:38:21 -0300

    increment userList component

---
+++ src/data/users.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    name: 'Henrique Nascimento',
+    email: 'henrique.mail@mail.com',
+    avatarUrl: 'https://imagensemoldes.com.br/wp-content/uploads/2021/04/Ilustracao-Rick-and-Morty-PNG.png'
+  },
+  {
+    id: 2,
+    name: 'Henrique Nascimento',
+    email: 'henrique.mail@mail.com',
+    avatarUrl: 'https://imagensemoldes.com.br/wp-content/uploads/2021/04/Foto-Rick-and-Morty-PNG.png'
+  },
+  {
+    id: 3,
+    name: 'Henrique Nascimento',
+    email: 'henrique.mail@mail.com',
+    avatarUrl: 'https://www.freepnglogos.com/uploads/rick-and-morty-png/rick-and-morty-non-toxic-rick-sanchez-18.png'
+  },
+  {
+    id: 4,
+    name: 'Henrique Nascimento',
+    email: 'henrique.mail@mail.com',
+    avatarUrl: 'https://www.freepnglogos.com/uploads/rick-and-morty-png/rick-and-morty-morty-face-kushmastafresh-deviantart-27.png'
+  },
+  {
+    id: 5,
+    name: 'Henrique Nascimento',
+    email: 'henrique.mail@mail.com',
+    avatarUrl: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/631e9029-5f0a-4e29-b36a-d5084ce15b39/de3is4s-dd3e36fd-31bb-48d1-8eb6-bee47c24b56d.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzYzMWU5MDI5LTVmMGEtNGUyOS1iMzZhLWQ1MDg0Y2UxNWIzOVwvZGUzaXM0cy1kZDNlMzZmZC0zMWJiLTQ4ZDEtOGViNi1iZWU0N2MyNGI1NmQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.aDBIP0xFj78flaPQdfkhr0G_Be31OxxbsnPLCasPZe4'
+  },
+]
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,9 +1,64 @@
 import React from "react"
-import {Text} from "react-native"
+import { Alert, FlatList, View } from "react-native"
+import { Button, Icon, ListItem, Avatar } from "react-native-elements"
+import users from '../data/users'
 
-const UserList = () => {
+const UserList = ( props ) => {
+
+  function getUserItem({item: user}) {
+    return(
+      <ListItem
+        key={user.id} 
+        bottomDivider
+        onPress={() => props.navigation.navigate('UserForm')}
+      >
+        <Avatar rounded source={{uri: user.avatarUrl}} />
+        <ListItem.Content>
+          <ListItem.Title>{user.name}</ListItem.Title>
+          <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+        </ListItem.Content>
+        <ListItem.Content right>
+          {getActions(user)}
+        </ListItem.Content>
+        {/* <ListItem.Chevron color='black'/> */}
+      </ListItem>
+    )
+  }
+
+  function getActions(user) {
+    return(
+      <Button
+        onPress={() => confirmUserDeletion(user)}
+        type='clear'
+        icon={
+          <Icon name='delete' size={25} color='red' />
+        }
+      />
+    )
+  }
+
+  function confirmUserDeletion(user) {
+    Alert.alert('Excluir Usuário', `Realmente deseja excluir ${user.name} da lista?`, [
+      {
+        text: 'Sim',
+        onPress() {
+
+        }
+      },
+      {
+        text: 'Não'
+      }
+    ])
+  }
+
   return (
-    <Text>UserList</Text>
+    <View>
+      <FlatList
+        keyExtractor={user => user.id.toString()}
+        data={users}
+        renderItem={getUserItem}
+      />
+    </View>
   )
 }
 
